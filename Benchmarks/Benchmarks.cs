4d55400c4488fb872ca6c7bdb14995aa99235adc
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2023-03-02 14:14:31 -0500

    update benchmarks

--- Benchmarks/Benchmarks.cs
+++ Benchmarks/Benchmarks.cs
@@ -1,25 +1,28 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Engines;
+using CommonTracerInterfaces;
 using Datadog.Trace.Events.Serializers;
 using Datadog.Trace.Events.Writers;
 
 namespace Benchmarks;
 
-[ShortRunJob]
-// [SimpleJob(RunStrategy.Monitoring)]
+//[ShortRunJob]
+[SimpleJob(RunStrategy.Monitoring)]
+//[SimpleJob(RunStrategy.Throughput)]
 [MemoryDiagnoser]
-[MinColumn, Q1Column, Q3Column, MaxColumn]
-[EventPipeProfiler(EventPipeProfile.CpuSampling)]
+[MinColumn, MedianColumn, MaxColumn]
 [GcServer(true)]
 public class Benchmarks
 {
-    private const int SpansPerTrace = 10;
+    private const int TraceCount = 10;
+    private const int SpansPerTrace = 5;
     private const int TagsPerSpan = 10;
 
     private KeyValuePair<string, string>[] _tags;
+
+    private ISpanEventWriter _eventWriter;
 
     [GlobalSetup]
     public void Setup()
@@ -30,51 +33,50 @@ public class Benchmarks
         {
             _tags[i] = new KeyValuePair<string, string>($"key{i:00}", $"value{i + 1:00}");
         }
+
+        var eventSerializer = new MessagePackSpanEventSerializer();
+        _eventWriter = new HttpSpanEventWriter(eventSerializer, "http://localhost:8126/v0.1/events");
     }
 
     [Benchmark(Baseline = true)]
     public async ValueTask SendFullTrace()
     {
-        var tracer = new FullTraceTracer(Datadog.Trace.Tracer.Instance);
+        ITracer tracer = new FullTraceTracer(Datadog.Trace.Tracer.Instance);
 
-        CreateTrace(tracer, "full trace chunk", spansPerTrace: SpansPerTrace, tagsPerSpan: TagsPerSpan);
+        for (int traceIndex = 0; traceIndex < TraceCount; traceIndex++)
+        {
+            CreateTrace(tracer, service: "benchmark-api-v0.4", spansPerTrace: SpansPerTrace, _tags);
+        }
+
         await tracer.FlushAsync();
     }
 
     [Benchmark]
     public async ValueTask SendEventsTrace()
     {
-        var eventSerializer = new MessagePackSpanEventSerializer();
-        var eventWriter = new HttpSpanEventWriter(eventSerializer, "http://localhost:8127/v0.1/events");
-        var tracer = new EventTracer(new Datadog.Trace.Events.Tracer(eventWriter));
+        ITracer tracer = new EventTracer(new Datadog.Trace.Events.Tracer(_eventWriter));
 
-        CreateTrace(tracer, "events api", spansPerTrace: SpansPerTrace, tagsPerSpan: TagsPerSpan);
+        for (int traceIndex = 0; traceIndex < TraceCount; traceIndex++)
+        {
+            CreateTrace(tracer, service: "benchmark-api-events", spansPerTrace: SpansPerTrace, _tags);
+        }
+
         await tracer.FlushAsync();
     }
 
-    private void CreateTrace(ITracer tracer, string tracerType, int spansPerTrace, int tagsPerSpan)
+    private static void CreateTrace(ITracer tracer, string service, int spansPerTrace, KeyValuePair<string, string>[] tags)
     {
-        using (var root = tracer.StartSpan("root", "server", "my-service", "root-resource"))
+        using (var root = tracer.StartSpan("root", "server", service, "root-resource"))
         {
-            root.AddTag("tracer-type", tracerType);
-
             // add tags to root span
-            for (int tagIndex = 0; tagIndex < tagsPerSpan; tagIndex++)
-            {
-                (string key, string value) = _tags[tagIndex];
-                root.AddTag(key, value);
-            }
+            root.AddTags(tags);
 
             for (int spanIndex = 1; spanIndex < spansPerTrace; spanIndex++)
             {
-                using (var child = tracer.StartSpan("child", "server", "my-service", "child-resource"))
+                using (var child = tracer.StartSpan("child", "internal", service, "child-resource"))
                 {
                     // add tags to child span
-                    for (int tagIndex = 0; tagIndex < tagsPerSpan; tagIndex++)
-                    {
-                        (string key, string value) = _tags[tagIndex];
-                        child.AddTag(key, value);
-                    }
+                    child.AddTags(tags);
                 }
             }
         }
